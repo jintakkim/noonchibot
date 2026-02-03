9e7cc3b0e6a2a81dd051144d7dafefdb491f9e97
jintakkim <mine407603@gmail.com>
2026-02-03 14:27:43 +0900

    initial commit

---
+++ src/main/java/com/hotak/noonchibot/NoonchibotApplication.java
@@ -0,0 +1,13 @@
+package com.hotak.noonchibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoonchibotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoonchibotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotak/noonchibot/NoonchibotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotak.noonchibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoonchibotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
