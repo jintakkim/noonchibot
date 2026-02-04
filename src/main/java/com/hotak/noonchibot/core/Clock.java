1b2c2f4047f70d1dcb714c58988a72f5646d2526
jintakkim <mine407603@gmail.com>
2026-02-04 11:04:41 +0900

    Revert "Merge remote-tracking branch 'origin/feature/clock-tick-engine'"
    This reverts commit d3dc341a292910c04cd45c39545b5fb15c677954, reversing
    changes made to a7958988cea6f7903edf722f985db47e62208bc7.

--- src/main/java/com/hotak/noonchibot/core/Clock.java
+++
@@ -1,9 +0,0 @@
-package com.hotak.noonchibot.core;
-
-import java.time.Instant;
-
-public interface Clock {
-    void addIterator(TimeIterator iterator);
-    void removeIterator(TimeIterator iterator);
-    void runUntil(Instant endTime);
-}
