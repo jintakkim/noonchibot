64f83979aede85a8e5158ed158d101bf9b81e58f
jintakkim <mine407603@gmail.com>
2026-02-11 17:07:00 +0900

    feat: eventLogger 구현

---
+++ src/main/java/com/hotak/noonchibot/core/event/OrderFilledEvent.java
@@ -0,0 +1,25 @@
+package com.hotak.noonchibot.core.event;
+
+import com.hotak.noonchibot.core.datatype.OrderType;
+import com.hotak.noonchibot.core.datatype.PositionAction;
+import com.hotak.noonchibot.core.datatype.TradeType;
+import com.hotak.noonchibot.core.trade.fee.TradeFee;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record OrderFilledEvent(
+        Instant timestamp,
+        String orderId,
+        String pair,
+        TradeType tradeType,
+        OrderType orderType,
+        BigDecimal price,
+        BigDecimal amount,
+        TradeFee tradeFee,
+        String exchangeTradeId,
+        String exchangeOrderId,
+        Integer leverage,
+        PositionAction position
+) {
+}
