5cd5a1facb9a1000b3df5ff19ecd67dedc38b80f
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-14 00:24:20 +0900

    feat: Add getCurrentUnixTimestamp method in TimeUtils

--- src/main/java/com/hotak/noonchibot/core/utils/TimeUtils.java
+++ src/main/java/com/hotak/noonchibot/core/utils/TimeUtils.java
@@ -1,7 +1,13 @@
 package com.hotak.noonchibot.core.utils;
 
+import java.time.Instant;
+
 public class TimeUtils {
     public static double getCurrentSeconds() {
         return System.nanoTime() / 1_000_000_000.0;
+    }
+
+    public static double getCurrentUnixTimestamp() {
+        return Instant.now().toEpochMilli() / 1000.0;
     }
 }
