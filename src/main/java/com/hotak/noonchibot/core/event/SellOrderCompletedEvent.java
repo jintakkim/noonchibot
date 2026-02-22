d736d6aad685ad994e527b86ef6ad31881671612
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-22 17:52:16 +0900

    feat: Add SellOrderCompletedEvent

---
+++ src/main/java/com/hotak/noonchibot/core/event/SellOrderCompletedEvent.java
@@ -0,0 +1,17 @@
+package com.hotak.noonchibot.core.event;
+
+import com.hotak.noonchibot.core.order.OrderType;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record SellOrderCompletedEvent(
+        Instant timestamp,
+        OrderType type,
+        String tradingPair,
+        BigDecimal amount,
+        BigDecimal price,
+        String orderId,
+        Instant creationTimestamp,
+        String exchangeOrderId
+) {}
