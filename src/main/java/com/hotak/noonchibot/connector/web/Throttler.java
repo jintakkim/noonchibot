1ece083c20c5d31dec04b4df76951dbd9b876066
jintakkim <mine407603@gmail.com>
2026-02-24 14:58:14 +0900

    chore: 기존 사용하지 않는 Throttler 제거

--- src/main/java/com/hotak/noonchibot/connector/web/Throttler.java
+++
@@ -1,8 +0,0 @@
-package com.hotak.noonchibot.connector.web;
-
-import java.util.function.Supplier;
-
-@FunctionalInterface
-public interface Throttler {
-     <T> T execute(String limitId, Supplier<T> task);
-}
