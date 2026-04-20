e2629b00c9ee3b54ddf508e34905606b9d4d687d
junhocode <161407999+junhocode@users.noreply.github.com>
2026-04-20 12:15:32 +0900

    feat: Add BybitOrderExecutor

---
+++ src/main/java/com/hotak/noonchibot/connector/bybit/BybitOrderExecutor.java
@@ -0,0 +1,166 @@
+package com.hotak.noonchibot.connector.bybit;
+
+import com.hotak.noonchibot.connector.*;
+import com.hotak.noonchibot.connector.web.RestAssistant;
+import com.hotak.noonchibot.connector.web.RestRequest;
+import com.hotak.noonchibot.connector.web.TimeSynchronizer;
+import com.hotak.noonchibot.core.IoExecutor;
+import com.hotak.noonchibot.core.MainExecutor;
+import com.hotak.noonchibot.core.datatype.TradeType;
+import com.hotak.noonchibot.core.datatype.TradingRule;
+import com.hotak.noonchibot.core.event.ExchangeEventPublisher;
+import com.hotak.noonchibot.core.order.InFlightOrder;
+import com.hotak.noonchibot.core.order.OrderTracker;
+import com.hotak.noonchibot.core.order.OrderType;
+import com.hotak.noonchibot.core.order.TimeInForce;
+import com.hotak.noonchibot.core.order.execute.AbstractExchangeOrderExecutor;
+import com.hotak.noonchibot.core.orderbook.OrderBookDataSource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
+import tools.jackson.databind.JsonNode;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class BybitOrderExecutor extends AbstractExchangeOrderExecutor {
+    private final TimeSynchronizer timeSynchronizer;
+    private final RestAssistant restAssistant;
+
+    public BybitOrderExecutor(
+            OrderIdGenerator orderIdGenerator,
+            OrderTracker orderTracker,
+            TradingRuleRegistry tradingRuleRegistry,
+            TradingPairSymbolRegistry tradingPairSymbolRegistry,
+            OrderBookDataSource orderBookDataSource,
+            TimeSynchronizer timeSynchronizer,
+            ExchangeEventPublisher exchangeEventPublisher,
+            RestAssistant restAssistant,
+            MainExecutor mainExecutor,
+            IoExecutor ioExecutor
+    ) {
+        super(
+                BybitApiSpec.PLATFORM_NAME,
+                orderIdGenerator,
+                orderTracker,
+                tradingRuleRegistry,
+                true,
+                BybitApiSpec.ORDER_ID_PREFIX,
+                BybitApiSpec.MAX_ORDER_ID_LENGTH,
+                tradingPairSymbolRegistry,
+                orderBookDataSource,
+                exchangeEventPublisher,
+                mainExecutor,
+                ioExecutor
+        );
+        this.timeSynchronizer = timeSynchronizer;
+        this.restAssistant = restAssistant;
+    }
+
+    @Override
+    public Set<OrderType> getSupportedOrderType(String tradingPair) {
+        TradingRule rule = tradingRuleRegistry.getTradingRule(tradingPair);
+        if (rule == null) throw new IllegalArgumentException("Unknown trading pair: " + tradingPair);
+        return rule.supportedOrderTypes();
+    }
+
+    @Override
+    public Set<TimeInForce> getSupportedTimeInForce() {
+        return Set.of(TimeInForce.FOK, TimeInForce.GTC, TimeInForce.IOC);
+    }
+
+    @Override
+    protected boolean isRequestExceptionRelatedToTimeSynchronizer(Exception e) {
+        String message = e.getMessage();
+        return message != null
+                && message.contains(String.valueOf(BybitApiSpec.TIMESTAMP_ERROR_CODE));
+    }
+
+    @Override
+    protected boolean isOrderNotFoundDuringCancellationException(Throwable e) {
+        String message = e.getMessage();
+        if (message == null) return false;
+
+        return message.contains(String.valueOf(BybitApiSpec.ORDER_NOT_EXIST_ERROR_CODE))
+                || message.contains(String.valueOf(BybitApiSpec.UNKNOWN_ORDER_ERROR_CODE));
+    }
+
+    @Override
+    protected OrderPlacedDto placeOrder(InFlightOrder inFlightOrder) {
+        String symbol = tradingPairSymbolRegistry.convertTradingPairToExchangeSymbol(inFlightOrder.getTradingPair());
+        String tradeTypeApiValue = inFlightOrder.getTradeType() == TradeType.BUY ? "Buy" : "Sell";
+        String orderTypeApiValue = orderTypeToApiValue(inFlightOrder.getOrderType());
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("category", "spot");
+        body.put("symbol", symbol);
+        body.put("side", tradeTypeApiValue);
+        body.put("orderType", orderTypeApiValue);
+        body.put("qty", inFlightOrder.getAmount().toPlainString());
+        body.put("orderLinkId", inFlightOrder.getClientOrderId());
+
+        if (inFlightOrder.getOrderType() == OrderType.LIMIT) {
+            body.put("price", inFlightOrder.getPrice().toPlainString());
+            if(!inFlightOrder.isPostOnly()) {
+                body.put("timeInForce", BybitApiSpec.TIME_IN_FORCE_API_VALUE.get(inFlightOrder.getTimeInForce()));
+            }
+        }
+
+        RestRequest request = RestRequest.builder()
+                .method(HttpMethod.POST)
+                .pathUrl(BybitApiSpec.ORDER_CREATE_PATH_URL)
+                .authRequired(true)
+                .body(body)
+                .build();
+
+        try {
+            JsonNode response = restAssistant.executeRequestAndGetJsonBody(request);
+            int retCode = response.path("retCode").asInt();
+
+            if (retCode == 0) {
+                String exchangeOrderId = response.path("result").path("orderId").asString();
+                return new OrderPlacedDto(exchangeOrderId, Instant.ofEpochMilli(timeSynchronizer.serverTime()));
+            } else if (retCode == 10002 || retCode == 10016) {
+                return new OrderPlacedDto("UNKNOWN", Instant.ofEpochMilli(timeSynchronizer.serverTime()));
+            } else {
+                String retMsg = response.path("retMsg").asString();
+                throw new RuntimeException("Bybit API Order Failed: [" + retCode + "] " + retMsg);
+            }
+        } catch (ExchangeApiException e) {
+            if (e.httpStatusCode == HttpStatusCode.valueOf(503) && e.getMessage().contains("Unknown error, please check your request or try again later.")) {
+                return new OrderPlacedDto("UNKNOWN", Instant.ofEpochMilli(timeSynchronizer.serverTime()));
+            }
+            throw e;
+        }
+    }
+
+    @Override
+    protected boolean placeCancel(String orderId, InFlightOrder trackedOrder) {
+        String symbol = tradingPairSymbolRegistry.convertTradingPairToExchangeSymbol(trackedOrder.getTradingPair());
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("category", "spot");
+        body.put("symbol", symbol);
+        body.put("orderLinkId", orderId);
+
+        RestRequest request = RestRequest.builder()
+                .method(HttpMethod.POST)
+                .pathUrl(BybitApiSpec.ORDER_CANCEL_PATH_URL)
+                .body(body)
+                .authRequired(true)
+                .build();
+
+        JsonNode response = restAssistant.executeRequestAndGetJsonBody(request);
+        int retCode = response.path("retCode").asInt();
+        return retCode == 0;
+    }
+
+    private static String orderTypeToApiValue(OrderType orderType) {
+        return switch (orderType) {
+            case LIMIT -> "Limit";
+            case MARKET -> "Market";
+            default -> throw new IllegalArgumentException("Unsupported OrderType: " + orderType);
+        };
+    }
+}
